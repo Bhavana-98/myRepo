420ac66366e976cf8dae8161c186577a3653c71c
Chennu <bhavana.chennu@dxc.com>
2020-08-25 09:56:33 +0530

    first commit

---
+++ TimeEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeEntry
+{
+    public class TimeEntry
+    {
+        public long ?Id { get; set; }
+        public long ProjectId { get; set; }
+        public long UserId { get; set; }
+        public DateTime Data { get; set; }
+        public int Hours { get; set; }
+
+        public TimeEntry(long id,long projectId,long userId,DateTime date,int hours)
+        {
+            Id = id;
+            ProjectId = projectId;
+            UserId = userId;
+            Data = Data;
+            Hours = hours;
+        }
+        public TimeEntry( long projectId, long userId, DateTime date, int hours)
+        {
+            Id = null;
+            ProjectId = projectId;
+            UserId = userId;
+            Data = Data;
+            Hours = hours;
+        }
+
+    }
+}
---
+++ WelcomeMsg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeEntry
+{
+    public class WelcomeMsg
+    {
+        public string Message { get; set; }
+        public WelcomeMsg(string message)
+        {
+            this.Message = message;
+        }
+    }
+}
